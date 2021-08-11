7221a3ef9f6c471759d5dbc8b5a24472583ba506
bharat <bharat@amwebtech.com>
2021-08-11 15:05:26 +0530

    working on separate config file

--- api-gateway-micro-service/server.js
+++ api-gateway-micro-service/server.js
@@ -4,7 +4,14 @@ const user = require("./../users-micro-service/bin/www");
 const crud = require("./../crud-micro-service/bin/www");
 const transaction = require("./../transaction-micro-service/bin/www");
 const os = require('os');
-console.log('OS.hostname()========= ', os.hostname());
-gateway()
-  .load(path.join(__dirname, 'config'))
-  .run();
+console.log('system username== ', os.hostname());
+// console.log('process', process.env.SERVER_USER_NAME);
+if (process.env.SERVER_USER_NAME === os.hostname()) {
+  gateway()
+    .load(path.join(__dirname, 'live-config'))
+    .run();
+} else {
+  gateway()
+    .load(path.join(__dirname, 'dev-config'))
+    .run();
+}
