87081bd214556a422ba65f5e6eed10b2899478cc
bharat <bharat@amwebtech.com>
2021-08-11 14:48:39 +0530

    working on separate config file

--- api-gateway-micro-service/server.js
+++ api-gateway-micro-service/server.js
@@ -3,6 +3,8 @@ const gateway = require('express-gateway');
 const user = require("./../users-micro-service/bin/www");
 const crud = require("./../crud-micro-service/bin/www");
 const transaction = require("./../transaction-micro-service/bin/www");
+const os = require('os');
+console.log('OS.hostname()========= ', os.hostname());
 
 gateway()
   .load(path.join(__dirname, 'config'))
